34d51800e89334bdb7c7140182c33748e994f913
Gabriel Torres <dev.gabriel.torres@gmail.com>
2024-11-22 20:34:57 -0300

    feat: bind TransactionCategory repository to its interface

--- src/app/Providers/RepositoryServiceProvider.php
+++ src/app/Providers/RepositoryServiceProvider.php
@@ -2,6 +2,8 @@
 
 namespace App\Providers;
 
+use App\Core\Domain\Repository\ITransactionCategoryRepository;
+use App\Persistence\Eloquent\Repository\TransactionCategoryRepository;
 use App\Persistence\Eloquent\Repository\UserRepository;
 use App\Core\Domain\Repository\IUserRepository;
 use Illuminate\Support\ServiceProvider;
@@ -11,5 +13,6 @@ class RepositoryServiceProvider extends ServiceProvider
     public function register(): void
     {
         $this->app->singleton(IUserRepository::class, UserRepository::class);
+        $this->app->singleton(ITransactionCategoryRepository::class,  TransactionCategoryRepository::class);
     }
 }
