cfc477c501d84c6ccd684ebd10b2c34da6143c22
Gabriel Torres <dev.gabriel.torres@gmail.com>
2024-11-23 20:13:22 -0300

    feat: add __get method to the Entity abstract class

--- src/app/Core/Domain/Entity/Entity.php
+++ src/app/Core/Domain/Entity/Entity.php
@@ -2,10 +2,21 @@
 
 namespace App\Core\Domain\Entity;
 
+use Exception;
 use Illuminate\Contracts\Support\Arrayable;
 
 abstract class Entity implements Arrayable
 {
+    public function __get($property)
+    {
+        if (isset($this->{$property})) {
+            return $this->{$property};
+        }
+
+        $className = get_class($this);
+        throw new Exception("Property {$property} not found in class {$className}");
+    }
+
     public function toArray(): array
     {
         return get_object_vars($this);
