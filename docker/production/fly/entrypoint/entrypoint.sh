cdd43e5d0f3b3d659b1a42eb37f11ad913ea3051
Gabriel Torres <dev.gabriel.torres@gmail.com>
2025-03-04 20:10:21 -0300

    Revert "refactor: simplify Fly.io deployment configuration"
    This reverts commit a480779a4c5d92bee3e7adcf1de700957992c35d.

--- docker/production/fly/entrypoint/entrypoint.sh
+++ docker/production/fly/entrypoint/entrypoint.sh
@@ -7,4 +7,9 @@ for f in /entrypoint/scripts/*.sh; do
 done
 chown -R www-data:www-data /var/www/html
 
-exec "$@"
+if [ $# -gt 0 ]; then
+    # If we passed a command, run it as root
+    exec "$@"
+else
+    exec supervisord -c /etc/supervisor/supervisord.conf
+fi
